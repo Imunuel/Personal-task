2c6633c99ff29a713d60df36c9193d877776d8ed
Imunuel <prokhorenko_roma@mail.ru>
2021-04-18 19:36:41 +0300

    created some models and added DRF

--- pdp/settings.py
+++ pdp/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'quizz.apps.QuizzConfig',
+    'rest_framework',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +41,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- quizz/models.py
+++ quizz/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+import datetime
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=100)
+    pub_date = models.DateField()
+
+    def __str__(self):
+        return self.question_text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=25)
+    points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
